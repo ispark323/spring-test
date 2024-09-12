b84b5e9cbc313f2dd29bfab38b0a6c7b280439e0
sungmin park <spark323@gmail.com>
2024-09-13 07:24:28 +1000

    edit Test

--- src/test/java/com/example/demo/SpringtestApplicationTests.java
+++ src/test/java/com/example/demo/SpringtestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -17,10 +18,11 @@ class SpringtestApplicationTests {
 
     @Test
     void testJpa() {
-    	Optional<Question> oq = this.questionRepository.findById(1);
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 }
